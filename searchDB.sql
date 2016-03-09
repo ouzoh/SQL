917d38ff36842f4cd5afca6b9de3670ef6244d68
Ogaga Uzoh <friendsofglory@gmail.com>
2016-03-09 13:33:27 -0500

    Update searchDB.sql

--- searchDB.sql
+++ searchDB.sql
@@ -2,3 +2,10 @@
 --Can be modified to like %
 SELECT TABSCHEMA,TABNAME FROM SYSCAT.COLUMNS WHERE COLNAME = 'column_name'
 WITH UR;
+
+-- 
+SELECT * FROM SYSCAT.COLUMNS
+WHERE COLNAME LIKE '%column_name%'
+AND TYPENAME LIKE '%data_type%'
+AND TABSCHEMA LIKE '%schema_name%'
+WITH UR;
