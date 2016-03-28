e9c236929c36023738b7363d4b466f72170bf138
Ogaga Uzoh <friendsofglory@gmail.com>
2016-03-28 11:29:48 -0400

    Update and rename TruncateTable to TruncateTable.sql

---
+++ TruncateTable.sql
@@ -0,0 +1,11 @@
+#Use with care
+#Ask for permission
+#Query the tables you need to truncate
+
+
+COMMIT;
+TRUNCATE TABLE #SCHEMANAME.TABLENAME# IMMEDIATE;
+
+
+#If the commit statement is not used in DB2, you should get an error
+#Immediate errors that rollback is not possible and data is not written in memory
