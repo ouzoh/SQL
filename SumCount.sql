f6455976e0ce60fe1de01cad0a2eba5b0900a4f8
Ogaga Uzoh <friendsofglory@gmail.com>
2016-03-28 16:05:49 -0400

    Update SumCount.sql

--- SumCount.sql
+++ SumCount.sql
@@ -1,6 +1,7 @@
-#Sum the number of rows of two tables
+#Sum the number of rows of multiple tables
 SELECT SUM(1) FROM
 (
 SELECT 1 FROM TABLE1 UNION ALL
 SELECT 1 FROM TABLE2 UNION ALL
+SELECT 1 FROM TABLE3
 )
